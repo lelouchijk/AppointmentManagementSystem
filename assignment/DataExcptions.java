f27aacbb1628ed3c0577ca60ba36d4d65ab2aed2
Pyae Phyo Aung <151609851+lelouchijk@users.noreply.github.com>
2025-07-10 21:02:00 +0630

    Delete assignment directory

--- assignment/DataExcptions.java
+++
@@ -1,9 +0,0 @@
-package assignment;
-
-public class DataExcptions extends Exception {
-	public DataExcptions() {
-		super("Please fill in the information completely");
-		
-		
-	}
-}
--- assignment/Queue.java
+++
@@ -1,9 +0,0 @@
-package assignment;
-
-public interface Queue<E> {
-	public int size();
-	public boolean isEmpty();
-	public E first();
-	public void enqeue (E e);
-	public E dequeue();
-}
